646e7462f2c270c71848dfb1f70588e905aecde1
Priit Kallas <kallaspriit@gmail.com>
2018-05-08 17:43:56 +0300

    Simplified.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,17 +1,9 @@
 #include <mbed.h>
 
-#include "SerialLogHandler.hpp"
-
 int main()
 {
-  // configure serial
-  Serial pc(USBTX, USBRX);
-  pc.baud(115200);
-
-  // set log handler to use along with the minimum level of detail to show
-  Log::setLogHandler(new SerialLogHandler(&pc, Log::LogLevel::DEBUG));
-  Log log = Log::getLog("main");
-  log.info("-- FIRMWARE V1.3 --");
+  // configure usb serial
+  Serial pc(USBTX, USBRX, 115200);
 
   // setup bluetooth serial
   Serial bt(p9, p10, 9600);
@@ -22,16 +14,11 @@ int main()
   // main loop
   while (true)
   {
-    log.info("test %d!", counter);
-
+    pc.printf("hello %d\n", counter);
     bt.printf("hello %d\n", counter);
-
-    // Thread::wait(1000);
 
     wait(1.0f);
 
     counter++;
-
-    // put your main code here, to run repeatedly:
   }
 }
