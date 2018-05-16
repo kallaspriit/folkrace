736770e76ce323d1a3e23e111b60e992f1d685c3
Priit Kallas <kallaspriit@gmail.com>
2018-05-16 15:21:18 +0300

    Implemented basic app fullscreen webview.

--- app/folkbot/src/main/java/kallaspriit/bot/MainActivity.java
+++ app/folkbot/src/main/java/kallaspriit/bot/MainActivity.java
@@ -2,6 +2,7 @@ package kallaspriit.bot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.example.com");
     }
 }
