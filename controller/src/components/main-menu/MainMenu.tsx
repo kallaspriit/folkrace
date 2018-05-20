b4dcee2620a9c2e5b525fdc9c412862f0f1d9799
Priit Kallas <kallaspriit@gmail.com>
2018-05-20 22:12:05 +0300

    Started with status view.

--- controller/src/components/main-menu/MainMenu.tsx
+++ controller/src/components/main-menu/MainMenu.tsx
@@ -4,45 +4,45 @@ import "./MainMenu.scss";
 
 const MainMenu: React.SFC<{}> = () => (
   <div className="main-menu">
-    <ul className="main-menu--nav">
+    <ul className="main-menu__nav">
       <li>
         <NavLink to="/status" activeClassName="active">
-          <div className="main-menu--nav--icon">
+          <div className="main-menu__nav__icon">
             <i className="icon icon__status" />
           </div>
-          <div className="main-menu--nav--text">Status</div>
+          <div className="main-menu__nav__text">Status</div>
         </NavLink>
       </li>
       <li>
         <NavLink to="/map" activeClassName="active">
-          <div className="main-menu--nav--icon">
+          <div className="main-menu__nav__icon">
             <i className="icon icon__map" />
           </div>
-          <div className="main-menu--nav--text">Map</div>
+          <div className="main-menu__nav__text">Map</div>
         </NavLink>
       </li>
       <li>
         <NavLink to="/remote" activeClassName="active">
-          <div className="main-menu--nav--icon">
+          <div className="main-menu__nav__icon">
             <i className="icon icon__remote" />
           </div>
-          <div className="main-menu--nav--text">Remote</div>
+          <div className="main-menu__nav__text">Remote</div>
         </NavLink>
       </li>
       <li>
         <NavLink to="/ai" activeClassName="active">
-          <div className="main-menu--nav--icon">
+          <div className="main-menu__nav__icon">
             <i className="icon icon__ai" />
           </div>
-          <div className="main-menu--nav--text">AI</div>
+          <div className="main-menu__nav__text">AI</div>
         </NavLink>
       </li>
       <li>
         <NavLink to="/settings" activeClassName="active">
-          <div className="main-menu--nav--icon">
+          <div className="main-menu__nav__icon">
             <i className="icon icon__settings" />
           </div>
-          <div className="main-menu--nav--text">Settings</div>
+          <div className="main-menu__nav__text">Settings</div>
         </NavLink>
       </li>
     </ul>
--- controller/src/views/status/StatusView.tsx
+++ controller/src/views/status/StatusView.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 import "./StatusView.scss";
 
-const StatusView: React.StatelessComponent<{}> = () => <div className="view status-view">Status</div>;
+const StatusView: React.StatelessComponent<{}> = () => (
+  <div className="view status-view">
+    <div className="status-grid">
+      <div className="status-grid__item status-grid__item--bluetooth">Bluetooth</div>
+      <div className="status-grid__item status-grid__item--web-socket">Web socket</div>
+      <div className="status-grid__item status-grid__item--log">Log</div>
+    </div>
+  </div>
+);
 
 export default StatusView;
