3e81a971132742c6a41b736577ed79dce5b4537e
Priit Kallas <kallaspriit@gmail.com>
2018-11-07 09:11:29 +0200

    Implemented new sync script

--- app/scripts/sync.ts
+++ app/scripts/sync.ts
@@ -1,118 +1,89 @@
 import chalk from "chalk";
-import * as chokidar from "chokidar";
+// import * as chokidar from "chokidar";
 import * as fs from "fs-extra";
-import { debounce } from "lodash";
+// import { debounce } from "lodash";
 import * as path from "path";
 
-// files to sync
-const fileMap: { [x: string]: string } = {
-  "build/bundle.js": "../android/folkbot/src/main/res/raw/bundle.js",
-  "build/main.css": "../android/folkbot/src/main/res/raw/main.css",
-  "build/index.html": "../android/folkbot/src/main/res/raw/index.html"
-};
+// configure paths and file patterns
+const sourcePath = path.join(__dirname, "..", "build");
+const destinationPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "android",
+  "folkbot",
+  "src",
+  "main",
+  "res",
+  "raw"
+);
+const filesToCopy = ["index.html", "static"];
 
-// pass in -w or --watch to run in watch mode
-const useWatchMode =
-  process.argv.find(arg => ["-w", "--watch"].indexOf(arg) !== -1) !== undefined;
-
-if (useWatchMode) {
-  const watcher = chokidar.watch(["build/*.js", "build/*.css", "build/*.html"]);
-
-  // debounce running the synchronization
-  const runDebounce = debounce(async () => {
-    const startTime = Date.now();
-
-    try {
-      await sync();
-      const timeTaken = Date.now() - startTime;
-
-      process.stdout.write(
-        `${chalk.bgGreen.black(" DONE ")} in ${chalk.bold(
-          `${pad(timeTaken, 4)}ms`
-        )}\n`
-      );
-    } catch (error) {
-      const timeTaken = Date.now() - startTime;
-
-      showError(error, timeTaken);
-    }
-  }, 500);
-
-  watcher.on("all", async (event, filename) => {
-    process.stdout.write(
-      chalk.reset(
-        `> ${chalk.bold(pad(event, 6))} for ${chalk.bold(
-          pad(filename, 30)
-        )}, scheduling sync\n`
-      )
-    );
-
-    await runDebounce();
-  });
-} else {
+(async () => {
   const startTime = Date.now();
 
-  sync()
-    .then(() => {
-      const timeTaken = Date.now() - startTime;
+  await sync();
 
-      process.stdout.write(
-        `${chalk.bgGreen.black(" DONE ")} in ${chalk.bold(
-          `${pad(timeTaken, 4)}ms`
-        )}\n`
-      );
-    })
-    .catch(error => {
-      const timeTaken = Date.now() - startTime;
-
-      showError(error, timeTaken);
-    });
-}
-
-async function sync(): Promise<void> {
-  const sources = Object.keys(fileMap);
-  const basePath = path.join(__dirname, "..", "..", "controller");
-
-  for (const source of sources) {
-    const destination = fileMap[source];
-
-    try {
-      await fs.copy(
-        path.join(basePath, source),
-        path.join(basePath, destination)
-      );
-    } catch (error) {
-      process.stdout.write(`failed - ${error.message}\n`);
-    }
-  }
-
-  // await fs.copy(
-  //   path.join(__dirname, "..", "src", "index.html"),
-  //   path.join(__dirname, "..", "..", "app", "folkbot", "src", "main", "res", "raw", "index.html"),
-  // );
-}
-
-function pad(value: string | number, padding: number) {
-  const str = typeof value === "string" ? value : value.toString();
-  const padLength = padding - str.length;
-
-  if (padLength < 1) {
-    return str;
-  }
-
-  return `${new Array(padLength + 1).join(" ")}${str}`;
-}
-
-function showError(error: Error, timeTaken: number) {
-  const paddedMessage = error.message
-    .split("\n")
-    .map(line => `    ${line}`)
-    .join("\n");
+  const timeTaken = Date.now() - startTime;
 
   process.stdout.write(
-    `${chalk.bgRed.white(" FAIL ")} in ${chalk.bold(
-      `${pad(timeTaken, 4)}ms`
-    )}\n`
+    `${chalk.bgGreen.black(" DONE ")} in ${chalk.bold(`${timeTaken}ms`)}\n`
   );
-  process.stdout.write(`${paddedMessage}\n\n`);
+})().catch(error => console.error(error));
+
+// pass in -w or --watch to run in watch mode
+// const useWatchMode =
+//   process.argv.find(arg => ["-w", "--watch"].indexOf(arg) !== -1) !== undefined;
+
+// if (useWatchMode) {
+//   const watcher = chokidar.watch(["build/*.js", "build/*.css", "build/*.html"]);
+
+//   // debounce running the synchronization
+//   const runDebounce = debounce(async () => {
+//     const startTime = Date.now();
+
+//     try {
+//       await sync();
+//       const timeTaken = Date.now() - startTime;
+
+//       process.stdout.write(
+//         `${chalk.bgGreen.black(" DONE ")} in ${chalk.bold(
+//           `${pad(timeTaken, 4)}ms`
+//         )}\n`
+//       );
+//     } catch (error) {
+//       const timeTaken = Date.now() - startTime;
+
+//       showError(error, timeTaken);
+//     }
+//   }, 500);
+
+//   watcher.on("all", async (event, filename) => {
+//     process.stdout.write(
+//       chalk.reset(
+//         `> ${chalk.bold(pad(event, 6))} for ${chalk.bold(
+//           pad(filename, 30)
+//         )}, scheduling sync\n`
+//       )
+//     );
+
+//     await runDebounce();
+//   });
+// } else {
+
+// }
+
+async function sync(): Promise<void> {
+  // delete all files in destination directory
+  await fs.emptyDir(destinationPath);
+
+  // copy all requested files
+  await Promise.all(
+    filesToCopy.map(filename => {
+      const src = path.join(sourcePath, filename);
+      const dest = path.join(destinationPath, filename);
+
+      return fs.copy(src, dest);
+    })
+  );
 }
