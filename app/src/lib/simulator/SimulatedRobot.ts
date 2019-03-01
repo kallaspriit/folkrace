fd5c93586be75f30cb4dfeb2b98235912ebb6428
Priit Kallas <kallaspriit@gmail.com>
2019-03-01 10:46:51 +0200

    Analysed simulated robot messages

--- app/src/lib/simulator/SimulatedRobot.ts
+++ app/src/lib/simulator/SimulatedRobot.ts
@@ -6,6 +6,30 @@ export interface SimulatedRobotOptions {
 
 export type MessageListenerFn = (message: string) => void;
 
+/*
+Messages to handle:
+1. voltage > [1]
+2. s:M1:M2 > [2]
+3. rpm:RPM > [3]
+4. lidar > [4]
+5. current > [5]
+6. ping > [6]
+7. state > [7.left,7.right,7.start,2,4,1,5,8]
+8. proxy:CMD
+
+Messages to report:
+1. voltage:VOLTAGE
+2. s:M1:M2
+3. rpm:RPM
+4. lidar:RUNNING:VALID:TARGET_RPM:CURRENT_RPM:MOTOR_PWM
+5. current:CURRENT_M1:CURRENT_M2:VALID
+6. pong
+7. button:NAME:STATE
+8: e:M1:M2 (encoder values)
+9. reset
+10: l:ANGLE1:DISTANCE1:QUALITY1:ANGLE2:DISTANCE2:QUALITY2:ANGLE3:DISTANCE3:QUALITY3:ANGLE4:DISTANCE4:QUALITY4
+*/
+
 export class SimulatedRobot {
   private readonly options: Required<SimulatedRobotOptions>;
   private readonly log: Logger;
