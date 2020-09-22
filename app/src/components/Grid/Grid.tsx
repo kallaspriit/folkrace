1b9b10c080d0042eec3b206a3cebd1b2bd99dd7e
Priit Kallas <kallaspriit@gmail.com>
2020-09-22 22:52:56 +0300

    Improved status view and grid

--- app/src/components/Grid/Grid.tsx
+++ app/src/components/Grid/Grid.tsx
@@ -1,10 +1,29 @@
 import classNames from "classnames";
-import React from "react";
+import React, { CSSProperties } from "react";
 import { FlexProps, Flex } from "../Flex/Flex";
 import styles from "./Grid.module.scss";
 
-export const Grid: React.FC<FlexProps> = ({ className, children, ...rest }) => (
-  <Flex className={classNames(styles.grid, className)} {...rest}>
-    {children}
-  </Flex>
-);
+export interface GridProps extends FlexProps {
+  rowsTemplate?: string;
+  columnsTemplate?: string;
+}
+
+export const Grid: React.FC<GridProps> = ({ rowsTemplate, columnsTemplate, className, style, children, ...rest }) => {
+  const augmentedStyle: CSSProperties = {
+    ...style,
+  };
+
+  if (rowsTemplate) {
+    augmentedStyle.gridTemplateRows = rowsTemplate;
+  }
+
+  if (columnsTemplate) {
+    augmentedStyle.gridTemplateColumns = columnsTemplate;
+  }
+
+  return (
+    <Flex className={classNames(styles.grid, className)} style={augmentedStyle} {...rest}>
+      {children}
+    </Flex>
+  );
+};
--- app/src/views/StatusView/StatusView.tsx
+++ app/src/views/StatusView/StatusView.tsx
@@ -3,10 +3,11 @@ import { Grid } from "../../components/Grid/Grid";
 import { SerialStatus } from "../../components/SerialStatus/SerialStatus";
 import { TransportStatus } from "../../components/TransportStatus/TransportStatus";
 import { View } from "../../components/View/View";
+import styles from "./StatusView.module.scss";
 
 export const StatusView: React.FC = () => (
   <View>
-    <Grid>
+    <Grid className={styles["status-grid"]}>
       <SerialStatus />
       <TransportStatus />
     </Grid>
